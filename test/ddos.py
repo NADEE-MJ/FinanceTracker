d9d4b3d093716bfb5f968daa52dede2d76afeb31
NADEE-MJ <nadeem.maida@gmail.com>
2022-01-30 12:37:53 -0800

    added ddos.py test which tries to overloading the flask server, 650 seems to be around the max before it stops working properly

---
+++ test/ddos.py
@@ -0,0 +1,76 @@
+import requests, threading
+
+class TestUser():
+    def __init__(self, password1, password2, email, username):
+        self.password1 = password1
+        self.password2 = password2
+        self.email = email
+        self.username = username
+        self.access_token = None
+        self.refresh_token = None
+
+    def login(self, access_token, refresh_token):
+        self.access_token = access_token
+        self.refresh_token = refresh_token
+    
+    def refresh_access_token(self, access_token):
+        self.access_token = access_token
+
+class ReuqestThread(threading.Thread):
+    def __init__(self, id, base, params):
+        threading.Thread.__init__(self)
+        self.id = id
+        self.base = base
+        self.params = params
+
+    def run(self):
+        # print(f'starting thread#{self.id}')
+        print(get_request(self.base, self.params).json())
+        # print(f'ending thread#{self.id}')
+
+def get_request(base, params):
+    return requests.get(base + 'stocks', json=params)
+
+def main():
+    base = "http://localhost:5000/"
+
+    user = TestUser('ddospassword', 'ddospassword', 'ddosemail@email.com', 'ddosusername')
+
+    params = {
+        'email': user.email,
+        'username': user.username,
+        'password1': user.password1,
+        'password2': user.password2,
+        'password': user.password1
+    }
+    response = requests.post(base + 'user', json=params)
+    print(response.json())
+
+    response = requests.put(base + 'user', json=params)
+    print(response.json())
+
+    data = {
+        'access_token': response.json().get('access_token', {}),
+        'refresh_token': response.json().get('refresh_token', {})
+    }
+    user.login(data['access_token'], data['refresh_token'])
+
+
+    params['access_token'] = user.access_token
+
+    threads = []
+    times_to_request = 650
+    for i in range(times_to_request):
+        threads.append(ReuqestThread(id=i, base=base, params=params))
+    
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+    
+    print('main thread done')
+
+
+if __name__ == '__main__':
+    main()
